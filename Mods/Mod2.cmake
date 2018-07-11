77778e71dcd0f0d7ffc05ccc32102607fca69403
kenkoooo <kenkou.n@gmail.com>
2018-07-11 19:32:20 +0900

    push

--- Mods/Mod2.cmake
+++ Mods/Mod2.cmake
@@ -1,4 +1,3 @@
 include_guard(GLOBAL)
-add_library(mod2 STATIC ${CMAKE_CURRENT_LIST_DIR}/mod2.cpp)
-
 include(${CMAKE_CURRENT_LIST_DIR}/Mod1.cmake)
+add_library(mod2 STATIC ${CMAKE_CURRENT_LIST_DIR}/mod2.cpp)
